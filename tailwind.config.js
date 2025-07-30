3222aa53bee5d5e26b7d18ac39aea2b1fd0bf7c7
LuaFerGG <luafergg@gmail.com>
2025-07-30 10:24:39 -0500

    feat: add custom ping animation to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pingOnce: {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '75%': { transform: 'scale(1.2)', opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        'ping-once': 'pingOnce 1s ease-out',
+      },
+    },
   },
   plugins: [],
 };
