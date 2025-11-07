dd8e6e21e07213d2124eff30ffad16b95bf50fa5
LuaFerGG <luafergg@gmail.com>
2025-11-07 17:43:01 -0500

    feat: enhance Tailwind CSS configuration with new font families and spacing utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontWeight: {
+        extrablack: '1000',
+      },
+      spacing: {
+        25: '6.25rem',
+        30: '7.5rem',
+        40: '8.5rem',
+        50: '9.5rem',
+        60: '15.5rem',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        alexandria: ['Alexandria', 'sans-serif'],
+      },
       keyframes: {
         expandNav: {
           '0%': { width: '5rem' },
