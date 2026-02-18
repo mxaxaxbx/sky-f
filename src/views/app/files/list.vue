d079cadfbea4d98f7658721e1fec03f8ee736c5a
miguel.arenas.servi <miguel.arenas@servinformacion.com>
2026-02-18 16:27:59 -0500

    refactor: enhance file and folder display logic with improved loading and no results handling

--- src/views/app/files/list.vue
+++ src/views/app/files/list.vue
@@ -1,148 +1,202 @@
 <template>
-  <div>
-    <!-- list -->
-    <div class="w-full pb-4 pt-0 px-0 mt-20 sm:mt-0 sm:pt-2 sm:px-3">
-      <h1 class="text-lg mt-8 sm:mt-0 font-semibold mb-4 ml-2 text-[var(--text)] hidden sm:block">Could Drive</h1>
+  <h1 class="text-lg mt-8 sm:mt-0 font-semibold mb-4 ml-2 text-[var(--text)] hidden sm:block">Could Drive</h1>
 
-      <!-- loading -->
-      <div v-if="loading" class="flex justify-center items-center">
-        <i class="fas fa-spinner fa-spin text-2xl"></i>
-      </div>
+  <!-- loading -->
+  <div v-if="loading" class="flex justify-center items-center">
+    <i class="fas fa-spinner fa-spin text-2xl"></i>
+  </div>
 
-      <!-- if not results -->
-      <div v-else-if="!results.data.length" class="flex justify-center items-center">
-        <p class="text-gray-500">No hay resultados</p>
-      </div>
+  <!-- if not results -->
+  <div v-else-if="!fileResults.data.length && !folderResults.data.length" class="flex justify-center items-center">
+    <p class="text-gray-500">No hay resultados</p>
+  </div>
 
-      <!-- results -->
+  <!-- folders -->
+  <div class="w-full my-4">
+    <h3 class="font-semibold text-xs sm:text-sm truncate text-left"> Folders </h3>
+    <div class="flex flex-wrap gap-2">
       <div
+        v-for="folder in folderResults.data"
+        :key="folder.id"
         class="
-          grid grid-cols-1 gap-2
-          text-[var(--text)]
-
-          sm:grid-cols-2 sm:gap-4
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-6
+          w-64
+          border border-[var(--border)]
+          rounded-2xl p-2
+          flex items-center justify-between
         "
       >
-        <!-- folders -->
-
-        <!-- results -->
-        <div
-          v-for="file in fileResults.data"
-          :key="file.id"
+        <router-link
+          :to="`/app/folders/${folder.id}`"
           class="
-            group
-            flex items-center justify-between
-            w-full
-            bg-[var(--bg-secondary)]
-            border border-[var(--border)]
-            rounded-2xl min-w-0
+            flex items-center
+            space-x-2
+            min-w-0 w-full overflow-hidden
+          "
+        >
+          <i class="fas fa-folder"></i>
+          <h3 class="font-semibold text-xs sm:text-sm truncate text-left">
+            {{ folder.name }}
+          </h3>
+        </router-link>
+        <Dropdown
+          :classes="[
+            'bg-[var(--bg-secondary)]',
+            'border border-[var(--border)]',
+            'rounded-2xl',
+            'absolute','-right-0', 'z-20',
+            dropdownPosition,
+            'w-48',
+            'sm:-right-2'
+          ]"
+        >
+          <template #trigger="{ toggle }">
+            <button
+              @click="toggleDropdown(toggle, $event)"
+              class="text-[var(--text-terceary)] w-6 h-10">
+              <i class="fas fa-ellipsis-v"></i>
+            </button>
+          </template>
+          <template #content="{ }">
+            <div class="flex flex-col gap-0.5 px-1.5 py-1 font-light text-sm text-[#868686]">
+              <router-link
+                :to="`/app/folders/details/${folder.id}`"
+                class="
+                  flex items-center justify-start
+                  rounded-xl
+                  px-2 py-1
+                  border border-transparent
+                  hover:bg-[var(--hover-bg)] hover:border-[var(--color-primary)]
+                  transition-colors duration-30
+                "
+              >
+                <img src="/icon/icon_details.svg" alt="download" class="h-4 mr-3" />
+                <span>info</span>
+              </router-link>
+            </div>
+          </template>
+        </Dropdown>
+      </div>
+    </div>
+  </div>
 
-            hover:bg-[var(--hover-bg)]
-            hover:border-[var(--hover-border)]
-            hover:shadow-[0_0_2px_1px_rgba(10,119,243,0.3)]
-            transition-colors duration-300
+  <!-- results -->
+  <div class="w-full my-4">
+    <h3 class="font-semibold text-xs sm:text-sm truncate text-left"> Files </h3>
+    <div class="flex flex-wrap gap-2">
+      <div
+        v-for="file in fileResults.data"
+        :key="file.id"
+        class="
+          flex flex-wrap space-x-2
+        "
+      >
+        <div
+          class="
+            w-64
+            border border-[var(--border)]
+            rounded-2xl
+            flex items-center justify-between
           "
         >
           <router-link
             :to="`/app/files/details/${file.id}`"
-            class="flex-1 min-w-0"
+            class="
+              flex items-center
+              space-x-2
+              min-w-0 w-full overflow-hidden
+            "
           >
-            <div class="flex items center justify-between m-1">
-              <div
-                class="
-                  flex items-center
-                  space-x-2
-                  min-w-0 w-full overflow-hidden
+            <div
+              class="
+                flex items-center
+                space-x-2
+                min-w-0 w-full overflow-hidden
+              "
+            >
+              <!-- icons -->
+              <img
+                v-if="file.contentType === 'application/pdf'"
+                src="/icon/icon-pdf.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="
+                  file.contentType === 'application/msword' ||
+                  file.contentType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
                 "
-              >
-                <!-- icons -->
-                <img
-                  v-if="file.contentType === 'application/pdf'"
-                  src="/icon/icon-pdf.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="
-                    file.contentType === 'application/msword' ||
-                    file.contentType === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                  "
-                  src="/icon/icon-doc.svg"
-                  alt="Word file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="
-                    file.contentType === 'application/vnd.ms-excel' ||
-                    file.contentType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-                  "
-                  src="/icon/icon-excel.svg"
-                  alt="Word file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="
-                    file.contentType === 'application/vnd.ms-powerpoint' ||
-                    file.contentType === 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-                  "
-                  src="/icon/icon-ppt.svg"
-                  alt="PowerPoint file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="/image\/(png|webp|gif|avif)/.test(file.contentType)"
-                  src="/icon/icon-png.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="file.contentType === 'image/svg+xml'"
-                  src="/icon/icon-svg.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="/image\/(jpeg|jpg|bmp|tiff|heic|heif|x-icon|vnd\.microsoft\.icon)/.test(file.contentType)"
-                  src="/icon/icon-img.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="/^video\//.test(file.contentType)"
-                  src="/icon/icon-video.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="file.contentType === 'application/zip'"
-                  src="/icon/icon-zip.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else-if="/^audio\//.test(file.contentType)"
-                  src="/icon/icon-audio.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                />
-                <img
-                  v-else
-                  src="/icon/icon-file.svg"
-                  alt="image file icon"
-                  class="h-10 w-10"
-                  />
-                <!-- title and date -->
-                <div class="flex-1 min-w-0">
-                  <h3 class="font-semibold text-xs sm:text-sm truncate text-left">
-                    {{ file.name }}
-                  </h3>
-                  <p class="text-[0.7rem] text-[var(--text-terceary)] font-light">
-                    {{ moment(file.created * 1000).format('DD/MM/YY HH:mm') }} - {{ formatFileSize(file.size) }}
-                  </p>
-                </div>
+                src="/icon/icon-doc.svg"
+                alt="Word file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="
+                  file.contentType === 'application/vnd.ms-excel' ||
+                  file.contentType === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+                "
+                src="/icon/icon-excel.svg"
+                alt="Word file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="
+                  file.contentType === 'application/vnd.ms-powerpoint' ||
+                  file.contentType === 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+                "
+                src="/icon/icon-ppt.svg"
+                alt="PowerPoint file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="/image\/(png|webp|gif|avif)/.test(file.contentType)"
+                src="/icon/icon-png.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="file.contentType === 'image/svg+xml'"
+                src="/icon/icon-svg.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="/image\/(jpeg|jpg|bmp|tiff|heic|heif|x-icon|vnd\.microsoft\.icon)/.test(file.contentType)"
+                src="/icon/icon-img.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="/^video\//.test(file.contentType)"
+                src="/icon/icon-video.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="file.contentType === 'application/zip'"
+                src="/icon/icon-zip.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else-if="/^audio\//.test(file.contentType)"
+                src="/icon/icon-audio.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <img
+                v-else
+                src="/icon/icon-file.svg"
+                alt="image file icon"
+                class="h-10 w-10"
+              />
+              <!-- title and date -->
+              <div class="">
+                <h3 class="font-semibold text-xs sm:text-sm truncate text-left">
+                  {{ file.name }}
+                </h3>
+                <p class="text-[0.7rem] text-[var(--text-terceary)] font-light">
+                  {{ moment(file.created * 1000).format('DD/MM/YY HH:mm') }} - {{ formatFileSize(file.size) }}
+                </p>
               </div>
             </div>
           </router-link>
