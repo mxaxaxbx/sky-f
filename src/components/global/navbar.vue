68af6de6e69d725a7b67acd590cc1e75093157a8
LuaFerGG <luafergg@gmail.com>
2026-05-14 20:14:19 -0500

    feat: create global navbar component with search and user dropdown functionality

--- src/components/global/navbar.vue
+++ src/components/global/navbar.vue
@@ -97,8 +97,8 @@
           'border border-[var(--border)]',
           'rounded-2xl', 'shadow-md',
           'absolute', 'z-20',
-          'sm:top-6', 'top-0',
-          'right-0', 'w-[96vw]', 'h-[98vh]', 'py-16 sm:h-auto sm:w-80 sm:py-8',
+          'sm:top-6', '-top-2',
+          '-right-2', 'w-screen', 'h-screen', 'py-16 sm:h-auto sm:w-80 sm:py-8',
         ]"
       >
         <template #trigger="{ toggle }">
